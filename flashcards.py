aa5169797df6a94e9ed896c1104784f33581f7fa
jay23cee <jayceexxiii@gmail.com>
2021-02-20 22:15:09 -0800

    Deleted count and datetime

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,14 +10,3 @@ def welcome():
     return "Welcome to my Flash Cards Application!"
 
 
-@app.route("/date")
-def date():
-    return "This page was served at " + str(datetime.now())
-
-
-counter =0
-@app.route("/count_views")
-def count_demo():
-    global counter
-    counter += 1
-    return "This page was served " + str(counter) + " times"
