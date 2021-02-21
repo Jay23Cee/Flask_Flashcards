c6fa503187e3852745628d56e9af319f481f4def
jay23cee <jayceexxiii@gmail.com>
2021-02-20 22:14:44 -0800

    added routes for date and count_demo

--- flashcards.py
+++ flashcards.py
@@ -1,8 +1,23 @@
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def welcome():
     return "Welcome to my Flash Cards Application!"
+
+
+@app.route("/date")
+def date():
+    return "This page was served at " + str(datetime.now())
+
+
+counter =0
+@app.route("/count_views")
+def count_demo():
+    global counter
+    counter += 1
+    return "This page was served " + str(counter) + " times"
